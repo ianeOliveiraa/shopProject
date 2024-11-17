a38587432fc258cbd7c0f2dd858e99abc603ad83
frezzx <pietro01patricia@gmail.com>
2024-11-16 21:23:02 -0400

    Implementação da Página de Vendas

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from .forms import ClienteForm, ProdutoForm
-from .models import Cliente, Produto
+from .forms import ClienteForm, ProdutoForm, VendaForm
+from .models import Cliente, Produto, Venda
 
 
 def cliente_list(request):
@@ -70,3 +70,24 @@ def excluir_produto(request, id):
     return render(request, 'confirmar_exclusao.html', {'produto': produto})
 
 
+
+
+def venda_list(request):
+    venda_list = Venda.objects.all()
+    paginator = Paginator(venda_list, 5)  # Define 5 clientes por página
+    page_number = request.GET.get('page')  # Obtém o número da página atual da URL
+    vendas = paginator.get_page(page_number)
+    return render(request, 'venda.html', {'vendas': vendas})
+
+
+
+def venda_form(request, id=None):
+    venda = get_object_or_404(Venda, id=id) if id else None
+    if request.method == 'POST':
+        form = VendaForm(request.POST, instance=venda)
+        if form.is_valid():
+            form.save()
+            return redirect('venda_list')
+    else:
+        form = VendaForm(instance=venda)
+    return render(request, 'venda_form.html', {'form': form})
